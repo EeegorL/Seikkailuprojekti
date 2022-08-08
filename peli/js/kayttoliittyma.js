6b5cbf131c48669b4e94e1b558a51a6ed7291ab3
Egor Legezin <egorlegezin@gmail.com>
2022-08-08 10:52:10 +0300

    testi

--- peli/js/kayttoliittyma.js
+++ peli/js/kayttoliittyma.js
@@ -37,7 +37,7 @@
         });
  
     }
-    console.log("testi");
+    console.log("testiii");
     function uusiPeli() {
         peli=new Peli();
         pohjoinen.addEventListener('click', ()=>{suoritaToiminto(Peli.SUUNTA.POHJOINEN);});
