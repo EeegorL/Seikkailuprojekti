ac8b537dc3122d867a1724aeccad0bfdb0017531
EeegorL <s2000998@edu.bc.fi>
2022-08-18 11:05:19 +0300

    Seihiin törmää, eikä niiden läpi enää pääse

--- canvasseikkailupeli/js/Peli.js
+++ canvasseikkailupeli/js/Peli.js
@@ -52,26 +52,37 @@ const vihollinen = new Hahmo({
 function teeSeinat(){
     const seinaP=new Seina({koord:{x:0,y:0},leveys:kanvaasi.width,korkeus:15,kanvaasi:k,vari:"red"})
         seinaP.piirra();//Pohjoisseinä
-    const seinaI=new Seina({koord:{x:0,y:0},leveys:15,korkeus:kanvaasi.height,kanvaasi:k,vari:"red"})
+    const seinaI=new Seina({koord:{x:0,y:0},leveys:15,korkeus:kanvaasi.height,kanvaasi:k,vari:"blue"})
         seinaI.piirra();//Itäseinä
-    const seinaE=new Seina({koord:{x:0,y:kanvaasi.height-15},leveys:kanvaasi.width,korkeus:15,kanvaasi:k,vari:"red"})
+    const seinaE=new Seina({koord:{x:0,y:kanvaasi.height-15},leveys:kanvaasi.width,korkeus:15,kanvaasi:k,vari:"yellow"})
         seinaE.piirra();//Eteläseinä 
-    const seinaL=new Seina({koord:{x:kanvaasi.width-15,y:0},leveys:15,korkeus:kanvaasi.height,kanvaasi:k,vari:"red"})
+    const seinaL=new Seina({koord:{x:kanvaasi.width-15,y:0},leveys:15,korkeus:kanvaasi.height,kanvaasi:k,vari:"green"})
         seinaL.piirra();//Länsiseinä
         seinat=[seinaE,seinaP,seinaL,seinaI];
 }
 
 function tarkistaTormaaminen(hahmo){
-    for(let seina of seinat){
+    //huoneiden seinät
         if(hahmo.menosuunta=="oikea"){
-            if(hahmo.koord.x==seina.koord.x && hahmo.koord.y==seina.koord.y){
-                console.log("isrjhj")
-                hahmo.kiihtyvyys.x=0;
+            if(hahmo.koord.x+hahmo.leveys>=seinat[2].koord.x){
+                hahmo.koord.x = seinat[2].koord.x-hahmo.leveys;        
+            }
         }
+        if(hahmo.menosuunta=="vasen"){
+            if(hahmo.koord.x-10<=seinat[3].koord.x){
+                hahmo.koord.x = seinat[3].koord.x+hahmo.leveys-35; 
+            }
         }
-
+        if(hahmo.menosuunta=="alas"){
+            if(hahmo.koord.y+hahmo.korkeus>=seinat[0].koord.y){
+                hahmo.koord.y = seinat[0].koord.y-hahmo.korkeus;        }
+        }
+        if(hahmo.menosuunta=="ylos"){
+            if(hahmo.koord.y-hahmo.korkeus+70<=seinat[1].koord.y){
+                hahmo.koord.y = seinat[1].koord.y+hahmo.korkeus-65;        }
+        }
     }
-}
+
 
 function moottori() {
     window.requestAnimationFrame(moottori);
