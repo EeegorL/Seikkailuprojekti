b7068e3840bd73f22ea72b53357c4685ae51e113
EeegorL <s2000998@edu.bc.fi>
2022-11-04 12:03:04 +0200

    Joosen huoneet lisätty, vihollis-insert muutettu auto_incrementiks (ei tarvii lisätä id:tä vaa sql pistää ite)

--- tietovarasto/insertLauseet.sql
+++ tietovarasto/insertLauseet.sql
@@ -3,4 +3,15 @@ use seikkailupeli;
 
 INSERT INTO huone (id,pohjoinen,etela,lansi,ita,nimi) VALUES ('4-1','4-2',NULL,NULL,NULL,'Tallinnanaukio');
 
-INSERT INTO vihollinen (id,hp,dmg,huone,nimi,x,y,nopeus,vari2,vari1,elossa,kuva) VALUES (1,100,10,'4-1','Itiksen Irmeli',400,250,1,'darkkhaki','brown',1,'irmeli.png')
+INSERT INTO vihollinen (hp,dmg,huone,nimi,x,y,nopeus,vari2,vari1,elossa,kuva) VALUES (100,10,'4-1','Itiksen Irmeli',400,250,1,'darkkhaki','brown',1,'irmeli.png')
+-- Vihollisen kuva pitää ekana ladata pelikansioon, eli mulle vaa discordin vaik discordin kautta nii pistän
+
+
+-- Joosen huoneet
+INSERT INTO huone (id,pohjoinen,etela,ita,lansi,nimi)VALUES ('4-1','4-2',NULL,NULL,NULL,'Tallinnanaukio');
+INSERT INTO huone (id,pohjoinen,etela,ita,lansi,nimi)VALUES ('4-2','4-3','4-1','5-2','3-2','Aula');
+INSERT INTO huone (id,pohjoinen,etela,ita,lansi,nimi)VALUES ('5-2',NULL,NULL,NULL,'4-2','Kentucky Fried Chicken');
+INSERT INTO huone (id,pohjoinen,etela,ita,lansi,nimi)VALUES ('3-2',NULL,NULL,'4-2','2-2','Käytävä');
+INSERT INTO huone (id,pohjoinen,etela,ita,lansi,nimi)VALUES ('2-2','2-3',NULL,'3-2',NULL,'Henkilökunnan tilat');
+INSERT INTO huone (id,pohjoinen,etela,ita,lansi,nimi)VALUES ('2-3','2-4',NULL,NULL,'1-3','Ovi');
+INSERT INTO huone (id,pohjoinen,etela,ita,lansi,nimi)VALUES ('1-3',NULL,NULL,'2-3',NULL,'Vartijan koppi');
