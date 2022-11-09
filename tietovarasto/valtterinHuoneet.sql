04aed20a03693926bae700d5cc6a3a5032dc130b
Lipponen Valtteri <s2001005@edu.bc.fi>
2022-11-09 12:02:27 +0200

    jiigejr

---
+++ tietovarasto/valtterinHuoneet.sql
@@ -0,0 +1,18 @@
+USE `seikkailupeli`;
+
+--   `id`
+--   `pohjoinen` ylös
+--   `etela` alas
+--   `lansi` vasen
+--   `ita` oikea
+--   `nimi`
+
+INSERT INTO `huone` VALUES ('4-3','4-4','4-2',NULL,NULL,'Prisma')
+INSERT INTO `huone` VALUES ('4-4',NULL,'4-3',NULL,NULL,'Varastotilat')
+INSERT INTO `huone` VALUES ('3-4','3-5',NULL,'2-4',NULL,'Jack&Jones')
+INSERT INTO `huone` VALUES ('2-4',NULL,'2-3',NULL,'3-4','Aukio')
+INSERT INTO `huone` VALUES ('3-5','3-6','3-4',NULL,NULL,'Käytävä')
+INSERT INTO `huone` VALUES ('3-6',NULL,'3-5',NULL,'4-6','Minibossi')
+
+
+-- tänne viel vissii sit ne viholliset
